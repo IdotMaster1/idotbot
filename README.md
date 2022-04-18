9c2ac843b59ddea7068754cd48b2fe139aa998c1
IdotMaster1 <justapersom@gmail.com>
2022-04-18 22:25:09 +0200

    who's deez?

---
+++ README.md
@@ -0,0 +1,40 @@
+### idotbot
+
+## Run it as a personal bot for your personal discord server like one with your friends, you can use it for public servers but its not that useful. Just follow the comments in the code and you will probably manage.
+
+Little project I made while school holidays, was fun.
+
+More useful bot: https://codeberg.org/phrl42/Tech-Bot
+might have stole some code from it :trollface:
+
+
+### How to Setup for Debian/Ubuntu based linux distro or Raspberry Pi
+
+# Install python3
+
+```
+sudo apt install python3
+```
+
+# Install dependencies
+
+```
+sudo python3 -m pip install discord.py
+```
+If this doesnt work, try doing this first then try again:
+```
+sudo apt install python3-pip
+```
+
+# Setup the bot
+
+Open in your favorite text editor and follow the comments
+
+# MAKE SURE YOU SET THE "PUBLIC BOT" OPTION TO OFF!
+
+# Start the bot
+
+```
+python3 main.py
+```
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import random
+import discord 
+from discord.ext import commands
+import discord.ext.commands 
+
+client = commands.Bot(command_prefix = '%') # % in front of the command, like %image or %help. We call this the prefix.
+
+# Commands 
+def user_is_idotmaster1(ctx):
+    return ctx.message.author.id == "your id here" # Currently useless, put your User ID here
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(f'XXXX')) # This is the status, like "Playing with XXXX"
+    print('Hello World! I am ready to serve you!')
+
+@client.command ()
+async def whoami(ctx):
+   await ctx.send(f'Heil {ctx.message.author.mention}, Thanks for using me!')
+
+@client.command ()
+async def ping(ctx):
+    await ctx.send(f'the balls ping: **{round(client.latency * 1000)}ms**')
+
+@client.command(pass_context=True)
+async def say(ctx, *, message: str):
+    if not ("@" in message):
+        await ctx.send(message)
+    else:
+        await ctx.send("stop trying to ping everyone <:trollcrazy:962783074544934962>") # This is an emoji from the bunkercoin discord. To make this work you need to include the ID. To get the full string do \:emojiname: and it will give you <:emojihere:XXXXXXXXXXXXXXXXXX>
+
+@client.command()
+async def image(ctx):
+    path = random.choice(os.listdir('holyarchive/'))  # If you are self-hosting, make a folder called holyarchive and stuff you want it to send IT HAS TO BE A VIDEO OR IMAGE
+    await ctx.send(file=discord.File("holyarchive/"+path))
+
+# Help command
+@client.command ()
+async def commands(ctx):
+    embed = discord.Embed(
+        colour = discord.Colour.blue()
+    )
+    embed.set_author(name='List of commands')
+    
+    embed.add_field(name='%whoami', value='Tells you your name, useless to the max', inline=False)
+    embed.add_field(name='%ping', value="Give you the ping of the bot", inline=False)
+    embed.add_field(name='%say', value="say!!", inline=False)
+    embed.add_field(name='%image', value="Best command, Gives you a random meme from my Holy Archive", inline=False)
+    embed.set_footer(text="Here to serve you!")
+    await ctx.send(embed=embed)
+
+client.run('TOKEN HERE')
